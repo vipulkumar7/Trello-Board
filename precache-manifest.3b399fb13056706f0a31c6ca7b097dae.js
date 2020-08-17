37171d225df101a5eabbd2430fda3773327682b3
vipulkumar7 <vipulkumar7@users.noreply.github.com>
2020-08-17 13:45:20 +0530

    Updates

---
+++ precache-manifest.3b399fb13056706f0a31c6ca7b097dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd9fca7c7abf52655a61aa107a856a0",
+    "url": "/Trello-Board/index.html"
+  },
+  {
+    "revision": "86dd993fabbae9f76c04",
+    "url": "/Trello-Board/static/js/2.18daa000.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Trello-Board/static/js/2.18daa000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d040ac0344d251a9980",
+    "url": "/Trello-Board/static/js/main.dcf3db73.chunk.js"
+  },
+  {
+    "revision": "77ff91bf42c73c9f49c5",
+    "url": "/Trello-Board/static/js/runtime-main.2bcb0824.js"
+  }
+]);
